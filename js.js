8c5a2d1045a8c23dce78b7698dccb62abe901569
przemek3woo <przemko.wo.fb@gmail.com>
2024-12-07 23:16:56 +0100

    Update js.js

--- js.js
+++ js.js
@@ -3,9 +3,9 @@ let pElement = document.getElementById("p")
 let body = document.body
 let i = 1
 let i2 = 0
-pElement.innerHTML = texts[0]
 let texts = [
     "Hejkaaa Milili 🐧🥰", "Jak taaam? 💋🤍", "Jak sie lulało? 🥺✨", "Jakiś sen?", "Przyśniłem Ci sie? 🫣🫣", "Kocham ciee 💋💍", "Nie budziłaś sie w nocy? 🥺🤍", "Opowiedz wszystko na messie 🤍", "Zobacz sobie który to nasz dzień 💍"]
+pElement.innerHTML = texts[0]
 body.addEventListener("click", () => {
     if (i === texts.length) {
         i2 += 1
