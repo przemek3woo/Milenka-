a60bbb9d0c9fed615c4688b78853d695970ffd3d
przemek3woo <przemko.wo.fb@gmail.com>
2024-12-07 23:22:18 +0100

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,16 @@
-
+let pElement = document.getElementById("p")
+let body = document.body
+let i = 1
+let i2 = 0
+let texts = [
+    "Hejkaaa Milili 🐧🥰", "Jak taaam? 💋🤍", "Jak sie lulało? 🥺✨", "Jakiś sen?", "Przyśniłem Ci sie? 🫣🫣", "Kocham ciee 💋💍", "Nie budziłaś sie w nocy? 🥺🤍", "Opowiedz wszystko na messie 🤍", "Zobacz sobie który to nasz dzień 💍"]
+pElement.innerHTML = texts[0]
+body.addEventListener("click", () => {
+    if (i === texts.length) {
+        window.location.href = "https://przemek3woo.github.io/ile-czasu/"
+    }
+    else {
+        pElement.innerHTML = texts[i]
+        i += 1
+    }
+})
